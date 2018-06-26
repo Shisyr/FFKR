c7200e1641b4109621fc07106b6c785e9b8fea3d
Ahmed Itaev <ahmeditaev@gmail.com>
2018-06-26 17:41:18 +0600

    added modal popup for video and fixed adaptive of fans page

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,17 @@ $(document).ready(function () {
             isClosed = false;
         }
     }
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
 });
 
 // tabbed content
